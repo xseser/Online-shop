d2173e82be2deb7f551ed3957732beb81db94f3f
xseser <mierzejewskiigor5@gmail.com>
2023-03-16 20:53:30 +0100

    added Promotion entity

---
+++ src/main/java/pl/put/poznan/shop/entity/Promotion.java
@@ -0,0 +1,33 @@
+package pl.put.poznan.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "promotions")
+public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "percent", nullable = false)
+    private Long percent;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    private Product product;
+}
