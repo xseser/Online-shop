fb91f426836a8daa10496b0e788efd6aa427ce83
xseser <mierzejewskiigor5@gmail.com>
2023-03-17 15:08:05 +0100

    Created ComplaintTypeRepository

---
+++ src/main/java/pl/put/poznan/shop/repository/ComplaintTypeRepository.java
@@ -0,0 +1,8 @@
+package pl.put.poznan.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.put.poznan.shop.entity.ComplaintType;
+@Repository
+public interface ComplaintTypeRepository extends JpaRepository<ComplaintType, Long> {
+}
